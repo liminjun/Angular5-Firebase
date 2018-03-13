9fb399622a75372bcaaaaabf730f77e5818b55fe
liminjun <v-leeli@hotmail.com>
2018-03-13 22:05:45 +0800

    feat:init codes.

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCxKEQ4z1sbUDfMob2rzp6X66e65r7q7Y8",
+    authDomain: "liminjun-friends.firebaseapp.com",
+    databaseURL: "https://liminjun-friends.firebaseio.com",
+    projectId: "liminjun-friends",
+    storageBucket: "",
+    messagingSenderId: "491543624524"
+  }
+};
